e776467578f033cd6ddf19fe474cb06bfeddfac6
superstark01 <superstark02@gmail.com>
2022-05-29 18:34:57 +0530

    123

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import AppBar from "./Components/AppBar";
-import "./App.css"
-import SidenavBar from "./Components/SidenavBar";
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from "@emotion/react";
-
-let theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FE0D0D',
-    },
-    secondary: {
-      main: '#edf2ff',
-    },
-  },
-});
-
-theme = createTheme(theme, {
-  palette: {
-    info: {
-      main: theme.palette.secondary.main,
-    },
-  },
-});
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './index.css';
+import Home from "./Pages/Home";
 
 
 function App() {
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <AppBar />
-        <div className="page-layout" >
-          <SidenavBar />
-        </div>
-      </ThemeProvider>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
